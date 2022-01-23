43bb99a34db35817dff0a5650301086a7fca95a3
pverbruggen <pieterjan.verbruggen@outlook.com>
2022-01-23 22:32:51 +0100

    Initial commit

---
+++ src/main/java/be/thomasmore/notaparty/NotapartyApplication.java
@@ -0,0 +1,13 @@
+package be.thomasmore.notaparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotapartyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotapartyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/thomasmore/notaparty/NotapartyApplicationTests.java
@@ -0,0 +1,13 @@
+package be.thomasmore.notaparty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotapartyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
